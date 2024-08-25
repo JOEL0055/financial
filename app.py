c8172e2b01848b4cc3055d496b284224a7ae6624
JOEL0055 <JOEL0055@e.ntu.edu.sg>
2024-08-25 20:56:34 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder="/Users/joelaiweithai/Downloads/templates")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    # Specify a different port using the 'port' keyword argument
+    app.run(port=5001)
